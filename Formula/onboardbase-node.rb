4f8a1e79d63ba9d7ebb0cf86a085e1516695b1f2
Olohundare Nasirudeen <iamnasirudeen@gmail.com>
2021-12-03 22:41:38 +0100

    upgraded nodejs version to node16

--- Formula/onboardbase-node.rb
+++ Formula/onboardbase-node.rb
@@ -1,8 +1,8 @@
 class OnboardbaseNode < Formula
     desc "node.js dependency for onboardbase"
     homepage "https://docs.onboardbase.com"
-    url "https://nodejs.org/download/release/v12.21.0/node-v12.21.0-darwin-x64.tar.xz"
-    version "12.21.0"
+    url "https://nodejs.org/download/release/v16.11.1/node-v16.11.1-darwin-x64.tar.xz"
+    version "16.11.1"
     sha256 "4184cc5412cdf256996aa7f559859abc355b48f03144349cf8531b6bf0526f49"
     keg_only "onboardbase-node is only used by Onboardbase CLI, which explicitly requires from Cellar"
   
