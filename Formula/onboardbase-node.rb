fe11b34590498c3a049d0f564a48025d27ac5f15
Olohundare Nasirudeen <iamnasirudeen@gmail.com>
2021-12-12 18:21:06 +0100

    updated nodejs12 SHA

--- Formula/onboardbase-node.rb
+++ Formula/onboardbase-node.rb
@@ -3,7 +3,7 @@ class OnboardbaseNode < Formula
     homepage "https://docs.onboardbase.com"
     url "https://nodejs.org/download/release/v12.21.0/node-v12.21.0-darwin-x64.tar.xz"
     version "12.21.0"
-    sha256 "ca25f6ebb10713cd545998bbc9d3715511e321d304b376574f3dd4ce9b70f75e"
+    sha256 "4184cc5412cdf256996aa7f559859abc355b48f03144349cf8531b6bf0526f49"
     keg_only "onboardbase-node is only used by Onboardbase CLI, which explicitly requires from Cellar"
   
     def install
