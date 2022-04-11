1ad4ae489a2a165008c74d7ba810b2828b1bae33
Olohundare Nasirudeen <iamnasirudeen@gmail.com>
2022-04-11 12:39:57 +0100

    feat: v1.5.3

--- Formula/onboardbase.rb
+++ Formula/onboardbase.rb
@@ -1,8 +1,8 @@
 class Onboardbase < Formula
     desc "Everything you need to get started with Onboardbase"
     homepage "https://docs.onboardbase.com"
-    url "https://onboardbase-cli.fra1.digitaloceanspaces.com/onboardbase-v1.5.2/onboardbase-v1.5.2.tar.gz"
-    sha256 "ac379b9ebbecb384e545648dbe45aecbeeff24038a3a9f6dddff1c43374414c2"
+    url "https://onboardbase-cli.fra1.digitaloceanspaces.com/onboardbase-v1.5.3/onboardbase-v1.5.3.tar.gz"
+    sha256 "5726d58f587f8d1bc04c1b06081a31a233b1ac044792e6684dd41d4f514e236f"
     depends_on "onboardbase/brew/onboardbase-node" => "12.21.0"
   
     def install
