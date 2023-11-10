28e70b8f017ba6497c4e8693d28b0a0ec6147f9e
Olohundare Nasirudeen <iamnasirudeen@gmail.com>
2023-11-10 13:07:24 +0100

    feat: v7.0.7

--- Formula/onboardbase.rb
+++ Formula/onboardbase.rb
@@ -1,8 +1,8 @@
 class Onboardbase < Formula
     desc "Everything you need to get started with Onboardbase"
     homepage "https://docs.onboardbase.com"
-    url "https://onboardbase-cli.fra1.digitaloceanspaces.com/onboardbase-v7.0.6/onboardbase-v7.0.6.tar.gz"
-    sha256 "41e3daa6bb7c3b46c0095332d8d3ba2273b71d25eee76dec3ec0de331fe1ee3f"
+    url "https://onboardbase-cli.fra1.digitaloceanspaces.com/onboardbase-v7.0.7/onboardbase-v7.0.7.tar.gz"
+    sha256 "03ee7428ba03f7e2b7d102f140eb3e8fdb4a924831cf882c551fa51594a6ebbd"
     depends_on "onboardbase/brew/onboardbase-node" => "12.21.0"
   
     def install
