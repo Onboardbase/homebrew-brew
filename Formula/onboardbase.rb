7ffd0ab745b01e439f32db3b103e430cd08cc94b
Aleem Isiaka <30846935+limistah@users.noreply.github.com>
2024-07-05 05:16:25 +0100

    Update onboardbase.rb

--- Formula/onboardbase.rb
+++ Formula/onboardbase.rb
@@ -2,7 +2,7 @@ class Onboardbase < Formula
     desc "Everything you need to get started with Onboardbase"
     homepage "https://docs.onboardbase.com"
     url "https://onboardbase-cli.fra1.digitaloceanspaces.com/onboardbase-v8.5.0/onboardbase-v8.5.0.tar.gz"
-    sha256 "b30bad3cb7eab9410543d84a8d0b5055cdc6cd49"
+    sha256 "915ed7b4789c9548a3c3e2689c364c739c5f6f8c"
     depends_on "onboardbase/brew/onboardbase-node" => "12.21.0"
   
     def install
